ad47c3cc2e8dd6f52fb5fec1e8fec0b69e47c379
devkya <kihaok@gmail.com>
2022-05-26 14:24:02 +0900

    serializers.py views.py 생성 urls.py router 생성

--- backend/instagram/urls.py
+++ backend/instagram/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet) # 2개의 url을 만들어줌
+print(router.urls)
+
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
--- backend/instagram/views.py
+++ backend/instagram/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+# def post_list(request):
+#     pass
+
+# def post_detail(request, pk):
+#     pass
